6a51ab7ad05131f0e378426c8804fbfcd232e50a
BzCoder <107630221@qq.com>
2018-09-03 20:37:35 +0800

    增加监听端的使用方法

--- src/main/java/com/example/ampq/service/amqpService.java
+++ src/main/java/com/example/ampq/service/amqpService.java
@@ -1,6 +1,9 @@
 package com.example.ampq.service;
 
 import com.example.ampq.bean.testBean;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +14,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class amqpService {
     @RabbitListener(queues = "springBoot.queue")
+    //2.自动创建队列 @RabbitListener(queuesToDeclare = {@Queue("myQueue"),@Queue("myQueue2")})
+    //3.自动创建 Exchange与Queue绑定
+    //@RabbitListener(bindings = @QueueBinding(
+    //        value = @Queue("myQueue"),
+    //        exchange = @Exchange("myExchange")
+    //))
     public void recevie(testBean bean) {
         System.out.println("监听到消息了");
         System.out.println(bean);
